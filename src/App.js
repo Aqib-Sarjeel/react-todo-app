030f2be347b047cd026b96e823e9ce0269c90c33
Aqib Sarjeel <iamaqibsarjeel@gmail.com>
2020-11-13 17:00:34 +0530

    my first to do app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css"
+
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+  addItem(todoValue){
+if(todoValue!==""){
+  const newItem={
+    id:Date.now(),
+    value:todoValue,
+    isDone:false
+  };
+  const list=[...this.state.list]
+  list.push(newItem);
+  this.setState({
+    list,
+    newItem:""
+  });
+}
+  }
+
+
+  deleteItem(id){
+   const list=[...this.state.list];
+  const updatedList=list.filter(item=>item.id!==id);
+  this.setState({list:updatedList})
+  }
+
+  updateInput(input){
+this.setState({newItem:input});
+  }
+render(){
+  return(
+
+    <div className="item">
+Add here.. <br/>
+<input
+
+type="text"
+className="input-text"
+placeholder="Add your Todos..."
+required
+value={this.state.newItem}
+onChange={e=>this.updateInput(e.target.value)}
+/>
+<button
+onClick={()=>this.addItem(this.state.newItem)}
+disabled={!this.state.newItem.length}
+className="add-btn">Add Todo</button>
+<div className="list">
+
+<ul className="back-ground">
+{
+ this.state.list.map(item=>{
+    return(
+      <li key={item.id}>
+
+        <input type="checkbox"
+        name="isDone"
+       
+        checked={item.isDone}
+        onChange={()=>{}}/>
+        {item.value}
+        <button
+        className="dlt-btn"
+        onClick={()=>this.deleteItem(item.id)}
+        
+        >X</button>
+
+      </li>
+    )
+  })
+}
+
+  <li className="ulist">
+
+<input className="input-box" type="checkbox"/>
+go to gym
+<button className="dlt-btn">X</button>
+
+  </li>
+</ul>
+
+
+</div>
+
+
+
     </div>
   );
 }
+
+
+};
+
 
 export default App;
